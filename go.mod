21eb31b0190e095483af6fd02450862d4757cf63
bfu4 <bella@sleeeepyti.me>
2021-07-19 16:10:09 -0400

    errrrr

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golisten
+
+go 1.16
--- tests/event_test.go
+++ tests/event_test.go
@@ -1,22 +1,23 @@
-package event
+package tests
 
 import (
+	"golisten/event"
 	"testing"
 )
 
 // TestEventEmission test the emission of events
 func TestEventEmission(t *testing.T) {
-	bus := CreateBus(true)
+	bus := event.CreateBus(true)
 	eventName := "test-event"
-	l := Listener{
-		On: func(e *Event) {
+	l := event.Listener{
+		On: func(e *event.Event) {
 			println(e.Name)
 			if e.Name != eventName {
 				t.Error("event names do not match!")
 			}
 		},
 	}
-	e := Event{
+	e := event.Event{
 		Name: eventName,
 		Id: 1,
 	}
